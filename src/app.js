a5c28c521be54766499d04af3ca21327d27681ff
Julio Facal <starlyn.dev@gmail.com>
2023-10-14 23:59:46 -0300

    base app

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const jogos = [
+    {
+        "id": 1,
+        "titulo": "CS2"
+    },
+    {
+        "id": 2,
+        "titulo": "Dota 2"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send("teste Node.js express");
+});
+
+app.get("/jogos", (req, res) => {
+    res.status(200).json(jogos);
+});
+
+app.post("/jogos", (req, res) => {
+    jogos.push(req.body);
+});
+
+export default app;
